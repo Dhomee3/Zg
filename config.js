a97629ecb49d89f06e9986eae5fd01a1830b4472
Dhomee3 <aljabryziad@gmail.com>
2026-06-22 10:39:40 +0300

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // الستاتس: online | idle | dnd | invisible
+  status: "idle",
+
+  // اسم اللعبة أو البرنامج
+  streamName: ".",
+
+  // رابط تويتش (مطلوب للنوع Streaming)
+  streamUrl: "https://www.twitch.tv/x20",
+
+  // السطر الأول (الوصف)
+  details: "",
+
+  // السطر الثاني (الحالة)
+  state: "",
+
+  // الصورة الكبيرة (رابط مباشر للصورة)
+  largeImage: "",
+  largeText: "",
+
+  // الصورة الصغيرة (رابط مباشر للصورة)
+  smallImage: "",
+  smallText: "",
+};
+    
