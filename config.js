0d3e6a10c2e4204761147e58c0083cc096cbed6b
Dhomee3 <aljabryziad@gmail.com>
2026-06-22 10:47:07 +0300

    Modify streamName and largeImage in config.js
    Updated streamName to an empty string and changed largeImage to a valid image URL.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   status: "idle",
 
   // اسم اللعبة أو البرنامج
-  streamName: ".",
+  streamName: " ",
 
   // رابط تويتش (مطلوب للنوع Streaming)
   streamUrl: "https://www.twitch.tv/x20",
@@ -15,7 +15,7 @@ module.exports = {
   state: "",
 
   // الصورة الكبيرة (رابط مباشر للصورة)
-  largeImage: "",
+  largeImage: "https://cdn.discordapp.com/attachments/1516638586622185482/1518522381004574770/IMG_0215.jpg?ex=6a3a39ac&is=6a38e82c&hm=8f45d051e978362b97e21fc978179702368a9ea250901f3fdf3fe1ced681217f&",
   largeText: "",
 
   // الصورة الصغيرة (رابط مباشر للصورة)
